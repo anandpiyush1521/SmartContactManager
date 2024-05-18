4c0be65ea2fda389ea749b1c578117503161174d
Piyush Anand <piyushanand2580@gmail.com>
2024-05-18 19:31:02 +0530

    Login with github

--- src/main/java/com/scm/config/OAuthAuthenticationSuccessHandler.java
+++ src/main/java/com/scm/config/OAuthAuthenticationSuccessHandler.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -35,6 +36,62 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
             Authentication authentication) throws IOException, ServletException {
         
         logger.info("OAuthAuthenticationSuccessHandler");
+
+        //identify the provider
+        var oauth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+
+        String authorizedClientRegistrationId =  oauth2AuthenticationToken.getAuthorizedClientRegistrationId();
+
+        logger.info("Registration id: "+authorizedClientRegistrationId);
+
+        var oauthUser =  (DefaultOAuth2User)authentication.getPrincipal();
+
+        oauthUser.getAttributes().forEach((key, value) -> {
+            logger.info("{} => {}", key, value);
+        });
+
+        User user = new User();
+        user.setUserId(UUID.randomUUID().toString()); 
+        user.setRoleList(List.of(AppConstants.ROLE_USER));
+        user.setEmailVerified(true);
+        user.setEnabled(true);
+        user.setPassword("dummy");
+
+        
+
+        if(authorizedClientRegistrationId.equalsIgnoreCase("google")){
+            // google
+            //google attributes
+            user.setEmail(oauthUser.getAttribute("email").toString());
+            user.setProfilePic(oauthUser.getAttribute("picture").toString());
+            user.setName(oauthUser.getAttribute("name").toString());
+            user.setProviderUserId(oauthUser.getName());
+            user.setAbout("This account is created by google.");
+
+
+        }else if(authorizedClientRegistrationId.equalsIgnoreCase("github")){
+            // github
+            //github attributes
+            String email = oauthUser.getAttribute("email") != null ? 
+                oauthUser.getAttribute("email").toString() : 
+                oauthUser.getAttribute("login").toString()+"@gmail.com";
+            
+            String picture = oauthUser.getAttribute("avatar_url").toString();
+            String name = oauthUser.getAttribute("login").toString();
+            String providerUserId = oauthUser.getName();
+
+            user.setEmail(email);
+            user.setProfilePic(picture);
+            user.setName(name);
+            user.setProviderUserId(providerUserId);
+            user.setProvider(Providers.GITHUB);
+            user.setAbout("This account is created using github.");
+
+        }else{
+            logger.info("OAuth2AuthenticationSuccesHandler: Unknown Provider");
+        }
+
+        /* 
 
         //save data in database
         DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
@@ -71,6 +128,15 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
         if(user2 == null){
             userRepo.save(user1);
             logger.info("User saved "+email);
+        }
+
+        */
+
+        //save the user
+        User user2 = userRepo.findByEmail(user.getEmail()).orElse(null);
+        if(user2 == null){
+            userRepo.save(user);
+            logger.info("User saved "+user.getEmail());
         }
         
         new DefaultRedirectStrategy().sendRedirect(request, response, "/user/profile");
