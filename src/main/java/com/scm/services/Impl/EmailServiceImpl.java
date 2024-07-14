81540491be3456ec5f35ea95d75c16b3d1b0d99c
Piyush Anand <piyushanand2580@gmail.com>
2024-07-14 11:41:41 +0530

    EmailServiceTest

--- src/main/java/com/scm/services/Impl/EmailServiceImpl.java
+++ src/main/java/com/scm/services/Impl/EmailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.scm.services.Impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,9 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender eMailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromAddress;
+
     @Override
     public void sendEmail(String to, String subject, String body) {
 
@@ -21,7 +25,7 @@ public class EmailServiceImpl implements EmailService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
-        message.setFrom("scm@demomailtrap.com");
+        message.setFrom(fromAddress);
 
         eMailSender.send(message);
 
