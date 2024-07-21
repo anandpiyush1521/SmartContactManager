f80334d634f559d60201ae95ade517722ac39fee
Piyush Anand <piyushanand2580@gmail.com>
2024-07-21 11:12:09 +0530

    User SideNav and links updated

--- src/main/java/com/scm/controllers/AuthController.java
+++ src/main/java/com/scm/controllers/AuthController.java
@@ -1,7 +1,5 @@
 package com.scm.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +19,6 @@ public class AuthController {
 
     @Autowired
     private UserRepo userRepo;
-
-    //private Logger logger = LoggerFactory.getLogger(AuthController.class);
     
     @GetMapping("/verify-email")
     public String verifyEmail(
